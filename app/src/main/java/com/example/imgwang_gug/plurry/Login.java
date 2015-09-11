125ea8aa8017b10194d51324cc74e241a76d69e4
임광국 <kwangkuk0821@gmail.com>
2015-09-11 11:38:19 +0900

    login 기능구현

--- app/src/main/java/com/example/imgwang_gug/plurry/Login.java
+++ app/src/main/java/com/example/imgwang_gug/plurry/Login.java
@@ -1,5 +1,6 @@
 package com.example.imgwang_gug.plurry;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
@@ -8,6 +9,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,6 +33,9 @@ import java.net.URL;
 
 public class Login extends AppCompatActivity {
 
+    private EditText email;
+    private EditText password;
+    private TextView signup;
     private static HttpURLConnection conn;
     private SharedPreferences pref;
     private final String prefName = "plurry";
@@ -36,7 +44,26 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        new AuthenticationTask().execute();
+
+        email = (EditText) findViewById(R.id.input_email);
+        password = (EditText) findViewById(R.id.input_password);
+        signup = (TextView) findViewById(R.id.link_to_register);
+        //new AuthenticationTask().execute();
+    }
+    //클릭 리스너
+    public void mOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin:
+                new AuthenticationTask().execute(
+                        "http://plurry.cycorld.com:3000/mobile/users/sign_in",
+                        email.getText().toString(),
+                        password.getText().toString());
+                break;
+            case R.id.link_to_register:
+                signup.setClickable(false);
+                Toast.makeText(Login.this, "후에 sign_up 으로 연결", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
     //값 불러오기
@@ -45,8 +72,9 @@ public class Login extends AppCompatActivity {
         String data = pref.getString(key, "");
         return data;
     }
+
     // 값 저장하기
-    private void savePreferences(String key, String value){
+    private void savePreferences(String key, String value) {
         pref = getSharedPreferences(prefName, MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         editor.putString(key, value);
@@ -54,14 +82,15 @@ public class Login extends AppCompatActivity {
     }
 
     // 값(Key Data) 삭제하기
-    private void removePreferences(String key){
+    private void removePreferences(String key) {
         pref = getSharedPreferences(prefName, MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         editor.remove(key);
         editor.commit();
     }
+
     // 값(ALL Data) 삭제하기
-    private void removeAllPreferences(){
+    private void removeAllPreferences() {
         pref = getSharedPreferences(prefName, MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         editor.clear();
@@ -69,6 +98,8 @@ public class Login extends AppCompatActivity {
     }
 
     public class AuthenticationTask extends AsyncTask<String, Void, String> {
+
+        ProgressDialog loginPending = new ProgressDialog(Login.this);
 
         public String jsonConverter(String str) {
             str = str.replace("\\", "");
@@ -80,6 +111,15 @@ public class Login extends AppCompatActivity {
         }
 
         @Override
+        protected void onPreExecute() {
+            loginPending.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            loginPending.setMessage("로그인 중 입니다...");
+
+            loginPending.show();
+            super.onPreExecute();
+        }
+
+        @Override
         protected String doInBackground(String... params) {
             URL url = null;
             String urlParameters = null;
@@ -88,14 +128,14 @@ public class Login extends AppCompatActivity {
             InputStream is = null;
             BufferedReader br = null;
             try {
-                url = new URL("http://plurry.cycorld.com:3000/mobile/users/sign_in");
-                urlParameters = "email=user@plurry.com&password=plurry";
+                url = new URL(params[0]);
+                urlParameters = "email=" + params[1] + "&password=" + params[2];
+                Log.d("parameters", urlParameters);
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
                 conn.setReadTimeout(5000);
                 conn.setConnectTimeout(5000);
                 conn.setRequestProperty("charset", "euc-kr");
-                conn.setRequestProperty("abc", "asd");
                 conn.setDoInput(true);
                 conn.setDoOutput(true);
 
@@ -105,7 +145,7 @@ public class Login extends AppCompatActivity {
 
                 int responseCode = conn.getResponseCode();
 
-                if(responseCode == HttpURLConnection.HTTP_OK) {
+                if (responseCode == HttpURLConnection.HTTP_OK) {
 
                     is = conn.getInputStream();
                     br = new BufferedReader(new InputStreamReader(is));
@@ -129,6 +169,7 @@ public class Login extends AppCompatActivity {
         }
 
         protected void onPostExecute(String data) {
+            loginPending.dismiss();
             // result is what you got from your connection
             JSONObject resultJSON = null;
             String result = null;
@@ -139,14 +180,17 @@ public class Login extends AppCompatActivity {
                 result = (String) resultJSON.get("result");
                 what = (String) resultJSON.get("what");
 
-                if(resultJSON.has("secret_token")) {
+                if (resultJSON.has("secret_token")) {
+                    Toast.makeText(Login.this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
                     secret_token = (String) resultJSON.get("secret_token");
                     savePreferences("secret_token", secret_token);
                     Intent i = new Intent(Login.this, MainActivity.class);
                     startActivity(i);
                     Login.this.finish();
+                } else {
+                    Toast.makeText(Login.this, "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
                 }
-                Log.d("task_result", "result = " + resultJSON );
+                Log.d("task_result", "result = " + resultJSON);
             } catch (JSONException e) {
                 Log.d("JSONException", "ERROR " + e.getMessage());
             }
@@ -154,7 +198,7 @@ public class Login extends AppCompatActivity {
     }
 
 
-        @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
